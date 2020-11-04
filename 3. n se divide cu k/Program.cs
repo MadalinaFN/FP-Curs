9adb175398657bd05e6e23d72e66377712dc002f
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2020-11-04 16:46:10 +0200

    n se divide cu k

---
+++ 3. n se divide cu k/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.n_se_divide_cu_k
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Determinati daca n se divide cu k, unde n si k sunt date de intrare.
+
+            int n, k, x;
+
+            Console.WriteLine("Introduceti valoarea pentru n");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti valoarea pentru k");
+            k = int.Parse(Console.ReadLine());
+
+            x = n / k;
+
+            if (n % k == 0)
+            {
+                Console.WriteLine($"N se divide cu K, iar rezultatul este {x}");
+            }
+            else
+            {
+                Console.WriteLine("N nu se divide cu K");
+            }
+        }
+    }
+}
