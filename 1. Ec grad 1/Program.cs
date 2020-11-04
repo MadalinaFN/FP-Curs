509a17283740474f199d1259c4efa7c9a4127902
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2020-11-04 16:15:27 +0200

    Ec grad 1

---
+++ 1. Ec grad 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Ec_grad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare.
+
+            int a, b, x;
+
+            Console.WriteLine("Introduceti valoarea pentru a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti valoarea pentru b");
+            b = int.Parse(Console.ReadLine());
+
+            x = - (b / a);
+
+            Console.WriteLine($"Rezultatul ecuatiei este {x}");
+        }
+    }
+}
