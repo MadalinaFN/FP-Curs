30aafa06d46cb6e0bfa26d59f39b139d3347b023
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2020-11-04 18:27:42 +0200

    Triunghi

---
+++ 6. Triunghi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Triunghi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Detreminati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi.
+        }
+    }
+}
